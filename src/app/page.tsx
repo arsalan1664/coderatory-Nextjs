9f42fac9d4e236d7d2e4caca118fa8cbb1503e33
arsalan1664 <119442314+arsalan1664@users.noreply.github.com>
2024-01-08 16:18:54 +0500

    Update Sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import TestEmailButton from './components/TestEmailButton';
+import { Sidebar } from './components/Sidebar';
 
 export default function Home() {
   const router = useRouter()
@@ -16,10 +17,14 @@ export default function Home() {
     }
 }, [status]);
   return (
-    <div className='w-full'>
-      <h1 className='text-3xl'>Home Page</h1>
-      <TestEmailButton/>
+    <div className="grid lg:grid-cols-6">
+    <Sidebar />
+    <div className="col-span-3 lg:col-span-5 lg:border-l">
+      <div className="h-full px-2 py-6 lg:px-2">
+        <h1 className='flex items-center justify-center h-[80vh] text-forground text-3xl' >App</h1>
+      </div>
     </div>
+  </div>
   )
 }
 

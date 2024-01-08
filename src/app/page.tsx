48fbf2249b59aa7c2899253db4b6a8f4aef321cd
arsalan1664 <119442314+arsalan1664@users.noreply.github.com>
2024-01-08 12:53:59 +0000

    protected routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,30 @@
 'use client'
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import TestEmailButton from './components/TestEmailButton';
+import { redirect, useRouter } from 'next/navigation';
 import { Sidebar } from './components/Sidebar';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
 
-export default function Home() {
+export default async function Home() {
   const router = useRouter()
-  const {status} = useSession()
+  const { status } = useSession()
   console.log(useSession());
-  
-  useEffect(() => {
-    if (status ==='unauthenticated') {
-        router.refresh();
-        router.push('/auth/signin');
-    }
-}, [status]);
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/auth/signin');
+  }
+
+
   return (
     <div className="grid lg:grid-cols-6">
-    <Sidebar />
-    <div className="col-span-3 lg:col-span-5 lg:border-l">
-      <div className="h-full px-2 py-6 lg:px-2">
-        <h1 className='flex items-center justify-center h-[80vh] text-forground text-3xl' >App</h1>
+      <Sidebar />
+      <div className="col-span-3 lg:col-span-5 lg:border-l">
+        <div className="h-full px-2 py-6 lg:px-2">
+          <h1 className='flex items-center justify-center h-[80vh] text-forground text-3xl' >App</h1>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
